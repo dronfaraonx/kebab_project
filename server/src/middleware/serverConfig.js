aa65637c3f9db34ca2bd990086e58610d6833eba
dron <dron@drons-MacBook-Pro.local>
2024-09-25 03:26:49 +0700

    added cors

--- server/src/middleware/serverConfig.js
+++ server/src/middleware/serverConfig.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 // const userSession = require('./getUser');
+
+const corsOptions = {
+ origin: ['http://localhost:5173'],
+ optionsSuccessStatus: 200,
+};
 
 const serverConfig = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(cookieParser());
   app.use(morgan('dev'));
-
+  app.use(cors(corsOptions));
+  
   // app.use(userSession);
 };
 
