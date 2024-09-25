5389a6f5d6550c6df7a888e6f03c28f7d0e129eb
Kirill <dronfaraonx@gmail.com>
2024-09-26 01:01:14 +0700

    added sessions and babel

--- server/server.js
+++ server/server.js
@@ -4,10 +4,6 @@ require('dotenv').config()
 const PORT = process.env.PORT || 8000;
 
 
-app.get('/', (req, res) => {
-    res.send('hello')
-})
-
 app.listen(PORT, () => {
   console.log('SERVER STARTED ON PORT', PORT);
 });
--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -12,4 +12,21 @@ router.get('/', async(req, res) => {
   }
 })
 
+router.post('/api/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const findUser = await User.findOne({ where: { email } });
+        // const hasSamePassword = await bcrypt.compare(password, findUser.password);
+        if(findUser && password == findUser.password) {
+            req.session.user_sid = findUser.id;
+            res.status(203).json({id: findUser.id});
+        } else {
+            res.status(401).json({message: 'Incorrect email or password'})
+        }
+    } catch (error) {
+        console.log(('ERROR WHILE LOGIN - ', error));
+        res.status(503).json({ message: 'SERVER ERROR' });
+    }
+  })
+
 module.exports = router;
